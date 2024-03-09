9f7b6a7b65437612a189200aa450e0b2358d8673
prashon <prashon109939@gmail.com>
2024-03-10 04:20:07 +0545

    new commit

---
+++ disc/index.js
@@ -0,0 +1,15 @@
+"use strict";
+class Students {
+    create() {
+        console.log("create");
+    }
+    read() {
+        console.log("read");
+    }
+    update() {
+        console.log("update");
+    }
+    delete() {
+        console.log("delete");
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Welcome to the Terminal!")
+interface ICRUD {
+    create(): void;
+    read(): void;
+    update(): void;
+    delete(): void;
+}
+
+class Students implements ICRUD {
+    create(): void {
+        console.log("create");
+    }
+    read(): void {
+        console.log("read");
+    }
+    update(): void {
+        console.log("update");
+    }
+    delete(): void {
+        console.log("delete");
+    }
+}
